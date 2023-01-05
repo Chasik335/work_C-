fad4b69069fb0dca562ccbe12fb9fff20f120cda
Vladislav <vhasovnikov@inbox.ru>
2023-01-05 23:53:59 +0500

    Написал код

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+int a = 5;
+int b =15;
+int max = 0;
+int min = 0;
+if (a > b)
+{
+    max = a;
+    min = b;
+}
+else
+{
+    max = b;
+    min = a;
+}
+Console.WriteLine("Наибольшее число -> " + max);
+Console.WriteLine("Наименьшее число -> " + min);
+
+
+
+
+
+
+
+
