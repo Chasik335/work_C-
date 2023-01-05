7439de099fed232cccd36ce064f99bbbdbbe58ee
Vladislav <vhasovnikov@inbox.ru>
2023-01-06 02:25:25 +0500

    Написал код, исправил схему, добавил инфу в readme

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+int N = 10;
+int[] arr = new int[N + 1];
+int index = 0;
+while (index < N + 1)
+{
+    if (index % 2 == 0)
+    {
+        arr[index] = index;
+        Console.WriteLine("Четное -> " +arr[index]);
+    }
+index = index +1;
+}
+
