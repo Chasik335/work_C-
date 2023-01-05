5d84672c6557dd66fb87e8bdefc33b91fdf6ccb4
Vladislav <vhasovnikov@inbox.ru>
2023-01-06 01:03:20 +0500

    Исправили блок схему, написали код, добавили инфу в readme

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+int[] arr = { 54, 32, 6 };
+int index = 0;
+int max = 0;
+while (index < arr.Length - 1)
+{
+    if (arr[index] > max)
+    {
+        max = arr[index];
+    }
+index = index + 1;
+}
+Console.WriteLine("Максимум -> " + max);
