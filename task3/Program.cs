1040e3c4129753183a52dba4379df614969f55fc
Vladislav <vhasovnikov@inbox.ru>
2023-01-06 01:31:12 +0500

    Написал код, добавил инфу в readme

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+int N = 7;
+if (N % 2 == 0)
+{
+    Console.WriteLine("Четное");
+}
+else
+{
+    Console.WriteLine("Нечетное");
+}
+
